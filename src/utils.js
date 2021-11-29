9622af91d90a36d63ed0521d2bac926ca4cffed4
Mykola Odnosumov <mkloouo@gmail.com>
2021-11-29 09:12:44 +0300

    finish bot

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,20 @@
-const axios = require("axios");
 const config = require("./config.json");
+const fetch = require("node-fetch");
 
 module.exports.getTodayExchangeRates = async () => {
   const date = new Date();
   date.setDate(date.getDate() - 1);
-  const response = await axios.get(
+  const response = await fetch(
     config.api.privatbank.url +
-      `/exchange_rates?json&date=${date.day}.${date.month}.${date.year}`
+      `/exchange_rates?json&date=${date.getUTCDate()}.${
+        date.getUTCMonth() + 1
+      }.${date.getUTCFullYear()}`
   );
-  return response.data;
+
+  return await response.json();
 };
 
-module.exports.sumUpTo = n => {
+module.exports.sumUpTo = (n) => {
   let sum = 0;
   for (let i = 0; i < n + 1; i++) {
     sum += i;
