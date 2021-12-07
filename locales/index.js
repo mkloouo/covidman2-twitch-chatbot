e7ff6279982556a536d6dafdd4ec67b1d199d221
Mykola Odnosumov <mkloouo@gmail.com>
2021-12-07 12:56:41 +0200

    add missing locale logic

--- locales/index.js
+++ locales/index.js
@@ -1,7 +1,13 @@
 const path = require('path');
 const { I18n } = require('i18n');
 
-module.exports.i18n = new I18n({
+const i18nInstance = new I18n({
   locales: ['en', 'ru'],
   directory: path.join(__dirname, '.'),
-}).__;
+  missingKeyFn: function (locale, value) {
+    return `${value}: either command or translation (${locale}) is not covered yet`;
+  },
+});
+
+module.exports.i18n = (phrase, locale = 'en') =>
+  i18nInstance.__({ phrase, locale });
