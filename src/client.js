9c1cb718ad6953821c4acca2b044d5734a5402d7
Mykola Odnosumov <mkloouo@gmail.com>
2021-12-10 01:22:14 +0200

    use oldest approach possible

--- src/client.js
+++ src/client.js
@@ -1,15 +1,11 @@
 const tmi = require('tmi.js');
 
-const { config } = require('./config');
-
 const opts = {
   options: {
     debug: true,
     skipMembership: true,
   },
   connection: {
-    server: '0.0.0.0',
-    port: Number(config.port),
     reconnect: true,
     secure: true,
   },
--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const process = require('process');
 
 module.exports.config = {
-  port: process.env.PORT || 5000,
   api: {
     privatbank: {
       url: ' https://api.privatbank.ua/p24api',
