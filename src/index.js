517728b771ef84c9928778009f04acac139cd055
Mykola Odnosumov <mkloouo@gmail.com>
2021-12-10 01:00:50 +0200

    revert to definite approach

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 require('dotenv').config();
-
-(async () => {
-  while (true) {
-    try {
-      await require('./bot').startBot();
-    } catch (e) {
-      console.log('[bot] error:', e);
-    }
-  }
-})().then((e) => console.log('[retry handler] error:', e));
+require('./bot').startBot();
